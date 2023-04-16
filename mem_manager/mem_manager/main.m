6a7dc328dcf99d651257c1b7d6e75cbb5545ea32
qishi20000 <qishi20000@gmail.com>
2023-04-16 10:51:42 +0800

    增加内存管理

---
+++ mem_manager/mem_manager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mem_manager
+//
+//  Created by qishi20000 on 2023/4/16.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
